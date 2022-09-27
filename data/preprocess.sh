9a99cbc7c22bc4ec577f785e26f608f04c5a2e9d
Yiheng Shu <yhshu@smail.nju.edu.cn>
2022-09-27 11:12:52 +0800

    Update preprocess.sh

--- data/preprocess.sh
+++ data/preprocess.sh
@@ -2,17 +2,17 @@
 
 # 1. get raw data
 echo "====> Step 1: download raw data"
-#mkdir RawData
+mkdir RawData
 cd RawData
 
-#wget https://www.dropbox.com/s/tohrsllcfy7rch4/SimpleQuestions_v2.tgz
-#tar -xzf SimpleQuestions_v2.tgz
+wget https://www.dropbox.com/s/tohrsllcfy7rch4/SimpleQuestions_v2.tgz
+tar -xzf SimpleQuestions_v2.tgz
 
-#wget https://www.dropbox.com/s/dt4i1a1wayks43n/FB5M-extra.tar.gz
-#tar -xzf FB5M-extra.tar.gz
+wget https://www.dropbox.com/s/dt4i1a1wayks43n/FB5M-extra.tar.gz
+tar -xzf FB5M-extra.tar.gz
 
-#wget http://nlp.stanford.edu/data/wordvecs/glove.6B.zip
-#tar -xzf glove.6B.zip
+wget http://nlp.stanford.edu/data/wordvecs/glove.6B.zip
+unzip glove.6B.zip
 mv glove.6B/glove.6B.300d.txt ../kbqa_data/
 
 #2. create entity2type type_voc rel_voc cpickle
@@ -27,6 +27,7 @@ python prepare_train_data.py
 echo "====> Step 4: rm dir RawData"
 rm -r RawData
 echo "====> Step 5: move generate data to kbqa_data"
+mkdir kbqa_data
 mv *.pickle kbqa_data/
 echo "data preprocess all done!"
 
